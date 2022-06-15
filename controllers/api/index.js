6cf4e61325f4b29f7714a5c8325f09aca0348419
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-06-15 13:56:14 -0600

    message.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,2 +1,8 @@
-const router = require("express").Router();
-module.exports = router;
+const express = require("express");
+
+const messageRouter = require("../../views/layout");
+const app = express();
+
+app.use("../ve", messageRouter);
+
+module.exports = app;
