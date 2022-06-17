35345b1dbaabf039ba72f141acaaab5327891b4e
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-06-17 14:19:23 -0600

    handlebars corrected

--- seeds/userData.js
+++ seeds/userData.js
@@ -3,19 +3,19 @@ const { User } = require("../models");
 const userData = [
   {
     id: 1,
-    name: "Sal",
+    username: "Sal",
     email: "sal@hotmail.com",
     password: "password12345",
   },
   {
     id: 2,
-    name: "Lernantino",
+    username: "Lernantino",
     email: "lernantino@gmail.com",
     password: "password12345",
   },
   {
     id: 3,
-    name: "Amiko",
+    username: "Amiko",
     email: "amiko2k20@aol.com",
     password: "password12345",
   },
