864cb1ff4872bfb7bb63646050ce7bbf1c672609
rrkincaid <rrkincaid@gmail.com>
2022-06-22 16:50:46 -0600

    added form for message input

--- controllers/api/contentRoutes.js
+++ controllers/api/contentRoutes.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
 
   // const contents = contentData.map((content) => content.get({ plain: true }));
 
-  res.render("homepage", { contentData });
+  res.render("messageform", { contentData });
   // res.status(200).json(contentData);
   //   catch (err) {
   //     res.status(500).json(err);
